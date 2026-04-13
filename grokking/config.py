8d40ca14e670aaca627d3f72997bf574e7c2c717
Mark C Kuzyk <mckuzyk@gmail.com>
2026-04-13 14:46:24 -0700

    add base class and TrainingConfig

--- grokking/config.py
+++ grokking/config.py
@@ -1,8 +1,25 @@
-from dataclasses import dataclass
+from pathlib import Path
+from dataclasses import dataclass, asdict
+from abc import ABC
+import json
+from datetime import datetime
 
 
 @dataclass
-class TransformerConfig:
+class Config(ABC):
+    def save(self, path):
+        with open(path, "w") as f:
+            json.dump(asdict(self), f, indent=2)
+
+    @classmethod
+    def load(cls, path):
+        with open(path, "r") as f:
+            kwargs = json.load(f)
+        return cls(**kwargs)
+
+
+@dataclass
+class TransformerConfig(Config):
     # Prime number to mod by
     P: int = 113
 
@@ -15,3 +32,23 @@ class TransformerConfig:
 
     # Dimension of hidden layer of MLP
     d_mlp: int = 512
+
+
+@dataclass
+class TrainConfig(Config):
+    save_path: str | None = None
+    random_seed: int = 42
+    epochs: int = 10_000
+    checkpoint_every: int = 100
+
+    # Train/Test data
+    train_frac: float = 0.3
+
+    # AdamW arguments
+    lr: float = 1e-3
+    weight_decay: float = 1.0
+
+    def __post_init__(self):
+        if self.save_path is None:
+            dir_name = datetime.now().strftime("run_%Y%m%d_%H%M%S")
+            self.save_path = str(Path("runs") / dir_name)
